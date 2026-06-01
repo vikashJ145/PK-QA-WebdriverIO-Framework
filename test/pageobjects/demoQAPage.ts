974f27be3928e023ce1ad0c0d188a6ef5cb5e87f
My_First_Repository <piyushkushwah4022@gmail.com>
2026-06-01 18:37:54 +0530

    Created New Script For DemoQA Site

---
+++ test/pageobjects/demoQAPage.ts
@@ -0,0 +1,42 @@
+import { $ } from '@wdio/globals'
+import Page from './basePage';
+
+
+class DemoQAPage extends Page {
+
+    public get elementsCard () {
+        return $("//h5[text()='Elements']");
+    }
+
+    public get textBoxOption () {
+        return $("//span[text()='Text Box']");
+    }   
+
+    public get userNameField () {
+        return $('//input[@id="userName"]');
+    }
+    public get emailField () {
+        return $('//input[@id="userEmail"]');
+    }
+
+    public async verifyAndClickElementTab () {
+        await this.elementsCard.scrollIntoView();
+        await this.elementsCard.waitForDisplayed({ timeout: 5000 });
+        await this.elementsCard.click();
+    }
+
+    public async verifyAndClickTextBoxOption () {    
+        await this.textBoxOption.waitForDisplayed({ timeout: 5000 });
+        await this.textBoxOption.click();
+    }
+
+    public async enterUserName (username: string) {
+        await this.userNameField.setValue(username);
+    }
+    
+    public async enterEmail (email: string) {   
+         await this.emailField.setValue(email);
+     }
+}
+
+export default new DemoQAPage();
